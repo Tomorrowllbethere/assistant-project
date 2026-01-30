466d16b2fb24d13dcf4ee2a0b18ddd2be2e6c024
Slytherin fantom <uliadiakiv@gmail.com>
2026-01-30 23:54:35 +0100

    Fixing and adding templates for cloud part

--- core/static/js/script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function() {
-    // Приховуємо екран завантаження
-    document.getElementById('loading-screen').style.display = 'none';
-
-    // Показуємо контент
-    document.getElementById('content').style.display = 'block';
-});
